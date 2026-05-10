570c96315b701aeb3e359fe19354301b6924e2e8
KNR1997 <kethakaranasinghe@gmail.com>
2026-05-10 16:29:00 +0530

    feat(enum): Asset status enum

---
+++ enums/asset_status.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class AssetStatus(str, Enum):
+    AVAILABLE = "Available"
+    CHECKED_OUT = "Checked Out"
+    BROKEN = "Broken"
+    ARCHIVED = "Archived"
+    READY_TO_DEPLOY = "Ready to Deploy"
+    PENDING = "Pending"
+    LOST_STOLEN = "Lost/Stolen"
+    OUT_FOR_DIAGNOSTICS = "Out for Diagnostics"
+    DEPLOYED = "Deployed"
